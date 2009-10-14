477d9c5c6bf9715d9f3dbf11a2e6b79026601597
karadog <KaraDog@gmail.com>
2009-10-14 09:12:37 +0000

    GPL v3 license terms added.

--- plugin/basictplhandler/bootstrap.php
+++ plugin/basictplhandler/bootstrap.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Copyright 2009 HouYu Li <karadog@gmail.com>
+ *
+ * This file is part of LoadingPlugins.
+ *
+ * LoadingPlugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LoadingPlugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LoadingPlugins.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if (!defined('LPLUGINS')) die('Access violation error!');
 
 function load_plugin_basictplhandler() {
@@ -20,7 +39,7 @@ function render_tpl($tpl_context) {
 
     $tpl_path = $GLOBALS['tpl_configs']['tpl_path'];
     $tpl_ext = $GLOBALS['tpl_configs']['tpl_ext'];
-    
+
     if ($GLOBALS['tpl_configs']['use_sysconf']) {
         $tpl_path = $GLOBALS['sys_configs']['tpl_path'];
         $tpl_ext = $GLOBALS['sys_configs']['tpl_ext'];
--- plugin/mysqli/active_object.php
+++ plugin/mysqli/active_object.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Copyright 2009 HouYu Li <karadog@gmail.com>
+ *
+ * This file is part of LoadingPlugins.
+ *
+ * LoadingPlugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LoadingPlugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LoadingPlugins.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if (!defined('LPLUGINS')) die('Access violation error!');
 
 /**
@@ -720,7 +739,7 @@ class ActiveObject {
     final public static function paginate($class_name, $where = false,
         $params = array(), $more_sql = false, $page_param = 'p') {
         self::_list_size();
-        
+
         $result = array();
 
         $record_num = self::count($class_name, $where, $params);
