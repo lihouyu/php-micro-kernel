fa5e5a7f9d2e2dab05fb7210ce55f3f661b79b21
KaraDog@gmail.com <KaraDog@gmail.com@0e55e55e-c819-11de-a077-756b9c6e95db>
2009-10-14 09:12:37 +0000

    GPL v3 license terms added.

--- trunk/plugin/basicdispatcher/bootstrap.php
+++ trunk/plugin/basicdispatcher/bootstrap.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * Copyright 2009 HouYu Li <karadog@gmail.com>
+ *
+ * This file is part of LoadingPlugins.
+ *
+ * LoadingPlugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LoadingPlugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LoadingPlugins.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if (!defined('LPLUGINS')) die('Access violation error!');
 
 function run_dispatch() {
@@ -14,7 +33,7 @@ function run_dispatch() {
         $app_path = $GLOBALS['sys_configs']['app_path'];
         $file_ext = $GLOBALS['sys_configs']['file_ext'];
     }
-    
+
     $module = get_var('m', 'GP', $default_module);
     include_once($my_dir.DS.$app_path.DS.$module.$file_ext);
 }
