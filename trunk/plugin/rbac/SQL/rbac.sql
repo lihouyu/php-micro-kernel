eb88e51eb09c05994a845cd4c8d6b8e4477bb61b
KaraDog@gmail.com <KaraDog@gmail.com@0e55e55e-c819-11de-a077-756b9c6e95db>
2009-10-30 13:17:21 +0000

    Fix table name prefix in ActiveObject::load_children & ActiveObject::load_parent. Add RBAC plugin, work in progress.

---
+++ trunk/plugin/rbac/SQL/rbac.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `{$prefix}{$user_class_name}_role`;
+CREATE TABLE `{$prefix}{$user_class_name}_role` (
+    `{$user_class_name}_id` int(11) NOT NULL,
+    `role_id` int(11) NOT NULL,
+    KEY (`{$user_class_name}_id`),
+    KEY (`role_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `{$prefix}roles`;
+CREATE TABLE `{$prefix}roles` (
+    `id` int(11) NOT NULL auto_increment,
+    `role_name` varchar(255) NOT NULL,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `{$prefix}assigned_actions`;
+CREATE TABLE `{$prefix}assigned_actions` (
+    `id` int(11) NOT NULL auto_increment,
+    `role_id` int(11) NOT NULL,
+    `actions` text NOT NULL,
+    PRIMARY KEY  (`id`),
+    KEY (`role_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
---
+++ trunk/plugin/rbac/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2009 HouYu Li <karadog@gmail.com>
+ *
+ * This file is part of LoadingPlugins.
+ *
+ * LoadingPlugins is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LoadingPlugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LoadingPlugins.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!defined('LPLUGINS')) die('Access violation error!');
+
+function load_plugin_rbac() {
+    $my_dir = dirname(__FILE__);
+    include_once($my_dir.DS.'rbac.config.php');
+}
+
+attach_event('onInitialize', 'rbac', 'load_plugin_rbac');
